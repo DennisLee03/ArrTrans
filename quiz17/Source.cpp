b496db0ab6a260c27e4b01e940bb4c7bf9595394
DennisLee03 <e64112287@gs.ncku.edu.tw>
2023-07-17 21:52:57 +0800

    加入專案檔案。

---
+++ quiz17/Source.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+void FillIn(int data[][5]);
+void Transpose(int data1[][5], int data2[][5]);
+void Process(int data[][5]);
+void Output(int data[][5]);
+void main() {
+	int data1[5][5], data2[5][5];
+	FillIn(data1);
+	Output(data1);
+	Transpose(data1, data2);
+	Output(data2);
+	Process(data2);
+	Output(data2);
+}
+void FillIn(int data[][5]) {
+	srand(87);
+	for (int i = 0;i < 5;i++)
+		for (int j = 0;j < 5;j++)
+			data[i][j] = 0 + rand() % (49 - 0);
+}
+void Transpose(int data1[][5], int data2[][5]) {
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5;j++) 
+			data2[i][j] = data1[j][i];
+}
+void Process(int data[][5]) {
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5;j++) {
+			if (data[i][j] < 25)
+				data[i][j] = -1 * data[i][j];
+			else data[i][j] = data[i][j];
+		}
+}
+void Output(int data[][5]) {
+	for (int i = 0;i < 5;i++) {
+		for (int j = 0;j < 5;j++)
+			printf("%3d ", data[i][j]);
+		printf("\n");}
+	printf("==========\n");
+}
+
